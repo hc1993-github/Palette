d32f93f099735640b6c8dc24b1145bd245ed9c4f
huachen <hc0507@126.com>
2022-08-01 10:43:30 +0800

    自定义view 雷达

--- app/src/main/java/com/example/palette/FourActivity.java
+++ app/src/main/java/com/example/palette/FourActivity.java
@@ -1,11 +1,8 @@
 package com.example.palette;
 
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.palette.view.ScaleOrMoveView;
 
 public class FourActivity extends AppCompatActivity {
 
@@ -15,7 +12,7 @@ public class FourActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_four);
-        ScaleOrMoveView scaleOrMoveView = findViewById(R.id.scaleView);
-        scaleOrMoveView.setImageBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.white));
+//        ScaleOrMoveView scaleOrMoveView = findViewById(R.id.scaleView);
+//        scaleOrMoveView.setImageBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.white));
     }
 }
