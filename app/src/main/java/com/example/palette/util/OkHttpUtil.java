e02d44b6173920c7e9c7abd550086601146af215
huachen <hc0507@126.com>
2022-11-15 15:52:41 +0800

    优化工具类

--- app/src/main/java/com/example/palette/util/OkHttpUtil.java
+++ app/src/main/java/com/example/palette/util/OkHttpUtil.java
@@ -30,11 +30,12 @@ public class OkHttpUtil {
     private volatile static OkHttpUtil okHttpUtil;
     private OkHttpClient client;
     private Handler handler;
-    private OkHttpUtil(){
+
+    private OkHttpUtil() {
         OkHttpClient.Builder builder = new OkHttpClient.Builder()
                 .connectTimeout(15, TimeUnit.SECONDS)
-                .writeTimeout(20,TimeUnit.SECONDS)
-                .readTimeout(20,TimeUnit.SECONDS);
+                .writeTimeout(20, TimeUnit.SECONDS)
+                .readTimeout(20, TimeUnit.SECONDS);
         client = builder
                 //.addInterceptor(new NoNetInterceptor())
                 //.eventListenerFactory(OkHttpEventListener.FACTORY)
@@ -42,10 +43,10 @@ public class OkHttpUtil {
         handler = new Handler(Looper.getMainLooper());
     }
 
-    public static OkHttpUtil getInstance(){
-        if(okHttpUtil==null){
-            synchronized (OkHttpUtil.class){
-                if(okHttpUtil==null){
+    public static OkHttpUtil getInstance() {
+        if (okHttpUtil == null) {
+            synchronized (OkHttpUtil.class) {
+                if (okHttpUtil == null) {
                     okHttpUtil = new OkHttpUtil();
                 }
             }
@@ -53,214 +54,214 @@ public class OkHttpUtil {
         return okHttpUtil;
     }
 
-    public void requestWithParams(String url,boolean isGet,Map<String,String> headerParams,Map<String,String> params,ResultCallback callback){
+    public void requestWithParams(String url, boolean isGet, Map<String, String> headerParams, Map<String, String> params, ResultCallback callback) {
         Request.Builder builder = new Request.Builder();
-        if(headerParams!=null && !headerParams.isEmpty()){
-            for(Map.Entry<String,String> entry:headerParams.entrySet()){
+        if (headerParams != null && !headerParams.isEmpty()) {
+            for (Map.Entry<String, String> entry : headerParams.entrySet()) {
                 builder.addHeader(entry.getKey(), entry.getValue());
             }
         }
         Request request;
-        if(isGet){
+        if (isGet) {
             String param = "";
-            if(params!=null && !params.isEmpty()){
-                for(Map.Entry<String, String> entry:params.entrySet()){
+            if (params != null && !params.isEmpty()) {
+                for (Map.Entry<String, String> entry : params.entrySet()) {
                     param += "&" + entry.getKey() + "=" + entry.getValue();
                 }
             }
-            if(!TextUtils.isEmpty(param)) url += "?" + param.substring(1);
+            if (!TextUtils.isEmpty(param)) url += "?" + param.substring(1);
             request = builder.url(url).build();
-        }else {
+        } else {
             FormBody.Builder form = new FormBody.Builder();
-            if(params!=null && !params.isEmpty()){
-                for(Map.Entry<String,String> entry:params.entrySet()){
+            if (params != null && !params.isEmpty()) {
+                for (Map.Entry<String, String> entry : params.entrySet()) {
                     form.add(entry.getKey(), entry.getValue());
                 }
                 request = builder.url(url).post(form.build()).build();
-            }else {
+            } else {
                 request = builder.url(url).post(form.build()).build();
             }
         }
         client.newCall(request).enqueue(new Callback() {
             @Override
-            public void onFailure(Call call,IOException e) {
-                onError("网络连接失败,请稍后重试",callback);
+            public void onFailure(Call call, IOException e) {
+                onNetError("网络连接失败,请稍后重试", callback);
             }
 
             @Override
-            public void onResponse(Call call,Response response) throws IOException {
-                if(response.isSuccessful() && response!=null){
-                    onSuccessCallback(response.body().string(),callback);
-                }else {
-                    onFailResponse(response.body().string(),callback);
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful() && response != null) {
+                    onSuccessCallback(response.body().string(), callback);
+                } else {
+                    onFailCallback(response.body().string(), callback);
                 }
             }
         });
 
     }
 
-    public void requestWithJson(String url,Map<String,String> headerParams,String json,ResultCallback callback){
+    public void requestWithJson(String url, Map<String, String> headerParams, String json, ResultCallback callback) {
         Request.Builder builder = new Request.Builder();
-        if(headerParams!=null && !headerParams.isEmpty()){
-            for(Map.Entry<String,String> entry:headerParams.entrySet()){
+        if (headerParams != null && !headerParams.isEmpty()) {
+            for (Map.Entry<String, String> entry : headerParams.entrySet()) {
                 builder.addHeader(entry.getKey(), entry.getValue());
             }
         }
         Request request;
-        if(!TextUtils.isEmpty(json)){
-            request = builder.url(url).post(RequestBody.create(json,MediaType.parse("application/json; charset=utf-8"))).build();
-        }else {
+        if (!TextUtils.isEmpty(json)) {
+            request = builder.url(url).post(RequestBody.create(json, MediaType.parse("application/json; charset=utf-8"))).build();
+        } else {
             request = builder.url(url).build();
         }
         client.newCall(request).enqueue(new Callback() {
             @Override
-            public void onFailure(Call call,IOException e) {
-                onError("网络连接失败,请稍后重试",callback);
+            public void onFailure(Call call, IOException e) {
+                onNetError("网络连接失败,请稍后重试", callback);
             }
 
             @Override
-            public void onResponse(Call call,Response response) throws IOException {
-                if(response.isSuccessful() && response!=null){
-                    onSuccessCallback(response.body().string(),callback);
-                }else {
-                    onFailResponse(response.body().string(),callback);
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful() && response != null) {
+                    onSuccessCallback(response.body().string(), callback);
+                } else {
+                    onFailCallback(response.body().string(), callback);
                 }
             }
         });
     }
 
-    public void requestWithParams(String url,boolean isGet,Map<String,String> headerParams,Map<String,String> params,ResultCallback callback,Interceptor... interceptors){
+    public void requestWithParams(String url, boolean isGet, Map<String, String> headerParams, Map<String, String> params, ResultCallback callback, Interceptor... interceptors) {
         Request.Builder builder = new Request.Builder();
-        if(headerParams!=null && !headerParams.isEmpty()){
-            for(Map.Entry<String,String> entry:headerParams.entrySet()){
+        if (headerParams != null && !headerParams.isEmpty()) {
+            for (Map.Entry<String, String> entry : headerParams.entrySet()) {
                 builder.addHeader(entry.getKey(), entry.getValue());
             }
         }
         Request request;
-        if(isGet){
+        if (isGet) {
             String param = "";
-            if(params!=null && !params.isEmpty()){
-                for(Map.Entry<String, String> entry:params.entrySet()){
+            if (params != null && !params.isEmpty()) {
+                for (Map.Entry<String, String> entry : params.entrySet()) {
                     param += "&" + entry.getKey() + "=" + entry.getValue();
                 }
             }
-            if(!TextUtils.isEmpty(param)) url += "?" + param.substring(1);
+            if (!TextUtils.isEmpty(param)) url += "?" + param.substring(1);
             request = builder.url(url).build();
-        }else {
+        } else {
             FormBody.Builder form = new FormBody.Builder();
-            if(params!=null && !params.isEmpty()){
-                for(Map.Entry<String,String> entry:params.entrySet()){
+            if (params != null && !params.isEmpty()) {
+                for (Map.Entry<String, String> entry : params.entrySet()) {
                     form.add(entry.getKey(), entry.getValue());
                 }
                 request = builder.url(url).post(form.build()).build();
-            }else {
+            } else {
                 request = builder.url(url).post(form.build()).build();
             }
         }
 
         OkHttpClient.Builder newBuilder = client.newBuilder();
-        for(Interceptor interceptor:interceptors){
+        for (Interceptor interceptor : interceptors) {
             newBuilder.addInterceptor(interceptor);
         }
         OkHttpClient okHttpClient = newBuilder.build();
         okHttpClient.newCall(request).enqueue(new Callback() {
             @Override
-            public void onFailure(Call call,IOException e) {
-                onError("网络连接失败,请稍后重试",callback);
+            public void onFailure(Call call, IOException e) {
+                onNetError("网络连接失败,请稍后重试", callback);
             }
 
             @Override
-            public void onResponse(Call call,Response response) throws IOException {
-                if(response.isSuccessful() && response!=null){
-                    onSuccessCallback(response.body().string(),callback);
-                }else {
-                    onFailResponse(response.body().string(),callback);
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful() && response != null) {
+                    onSuccessCallback(response.body().string(), callback);
+                } else {
+                    onFailCallback(response.body().string(), callback);
                 }
             }
         });
     }
 
-    public void requestDownLoadWithParams(String url, boolean isGet, Map<String,String> headerParams, Map<String,String> params, File file, ProgressListener listener, ResultCallback callback){
+    public void requestDownLoadWithParams(String url, boolean isGet, Map<String, String> headerParams, Map<String, String> params, File file, ProgressListener listener, ResultCallback callback) {
         Request.Builder builder = new Request.Builder();
-        if(headerParams!=null && !headerParams.isEmpty()){
-            for(Map.Entry<String,String> entry:headerParams.entrySet()){
+        if (headerParams != null && !headerParams.isEmpty()) {
+            for (Map.Entry<String, String> entry : headerParams.entrySet()) {
                 builder.addHeader(entry.getKey(), entry.getValue());
             }
         }
         Request request;
-        if(isGet){
+        if (isGet) {
             String param = "";
-            if(params!=null && !params.isEmpty()){
-                for(Map.Entry<String, String> entry:params.entrySet()){
+            if (params != null && !params.isEmpty()) {
+                for (Map.Entry<String, String> entry : params.entrySet()) {
                     param += "&" + entry.getKey() + "=" + entry.getValue();
                 }
             }
-            if(!TextUtils.isEmpty(param)) url += "?" + param.substring(1);
+            if (!TextUtils.isEmpty(param)) url += "?" + param.substring(1);
             request = builder.url(url).build();
-        }else {
+        } else {
             FormBody.Builder form = new FormBody.Builder();
-            if(params!=null && !params.isEmpty()){
-                for(Map.Entry<String,String> entry:params.entrySet()){
+            if (params != null && !params.isEmpty()) {
+                for (Map.Entry<String, String> entry : params.entrySet()) {
                     form.add(entry.getKey(), entry.getValue());
                 }
                 request = builder.url(url).post(form.build()).build();
-            }else {
+            } else {
                 request = builder.url(url).post(form.build()).build();
             }
         }
         OkHttpClient okHttpClient = client.newBuilder().addInterceptor(new ProgressInterceptor(listener)).build();
         okHttpClient.newCall(request).enqueue(new Callback() {
             @Override
-            public void onFailure(Call call,IOException e) {
-                onError("网络连接失败,请稍后重试",callback);
+            public void onFailure(Call call, IOException e) {
+                onNetError("网络连接失败,请稍后重试", callback);
             }
 
             @Override
-            public void onResponse(Call call,Response response) throws IOException {
-                if(response.isSuccessful() && response!=null){
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful() && response != null) {
                     long contentLength = response.body().contentLength();
-                    FileUtil.writeToFile(file.getAbsolutePath(),response.body().byteStream());
-                    if(file.length()==contentLength){
-                        onSuccessCallback(null,callback);
+                    FileUtil.writeToFile(file.getAbsolutePath(), response.body().byteStream());
+                    if (file.length() == contentLength) {
+                        onSuccessCallback(null, callback);
                     }
-                }else {
-                    onFailResponse(response.body().string(),callback);
+                } else {
+                    onFailCallback(response.body().string(), callback);
                 }
             }
         });
     }
 
-    public void requestUpLoadWithParams(String url,Map<String,String> headerParams, Map<String,String> bodyParams,File file,ProgressListener listener, ResultCallback callback){
+    public void requestUpLoadWithParams(String url, Map<String, String> headerParams, Map<String, String> bodyParams, File file, ProgressListener listener, ResultCallback callback) {
         Request.Builder builder = new Request.Builder();
-        builder.addHeader("Content-Type","multipart/form-data");
-        if(headerParams!=null && !headerParams.isEmpty()){
-            for(Map.Entry<String,String> entry:headerParams.entrySet()){
+        builder.addHeader("Content-Type", "multipart/form-data");
+        if (headerParams != null && !headerParams.isEmpty()) {
+            for (Map.Entry<String, String> entry : headerParams.entrySet()) {
                 builder.addHeader(entry.getKey(), entry.getValue());
             }
         }
         MultipartBody.Builder form = new MultipartBody.Builder();
-        form.addFormDataPart("file",file.getName(),RequestBody.create(file,MediaType.parse("application/octet-stream")));
-        if(bodyParams!=null && !bodyParams.isEmpty()){
+        form.addFormDataPart("file", file.getName(), RequestBody.create(file, MediaType.parse("application/octet-stream")));
+        if (bodyParams != null && !bodyParams.isEmpty()) {
             form.setType(MultipartBody.FORM);
-            for(Map.Entry<String,String> entry:bodyParams.entrySet()){
+            for (Map.Entry<String, String> entry : bodyParams.entrySet()) {
                 form.addFormDataPart(entry.getKey(), entry.getValue());
             }
         }
-        ProgressRequestBody requestBody = new ProgressRequestBody(form.build(),listener);
+        ProgressRequestBody requestBody = new ProgressRequestBody(form.build(), listener);
         Request request = builder.url(url).post(requestBody).build();
         OkHttpClient okHttpClient = client.newBuilder().build();
         okHttpClient.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(@NotNull Call call, @NotNull IOException e) {
-                onError("网络连接失败,请稍后重试",callback);
+                onNetError("网络连接失败,请稍后重试", callback);
             }
 
             @Override
             public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                if(response.isSuccessful() && response!=null){
-                    onSuccessCallback(response.body().string(),callback);
-                }else {
-                    onFailResponse(response.body().string(),callback);
+                if (response.isSuccessful() && response != null) {
+                    onSuccessCallback(response.body().string(), callback);
+                } else {
+                    onFailCallback(response.body().string(), callback);
                 }
             }
         });
@@ -268,7 +269,7 @@ public class OkHttpUtil {
 
     private void onSuccessCallback(String info, ResultCallback callback) {
         handler.post(() -> {
-            if(callback!=null){
+            if (callback != null) {
                 try {
                     callback.onSuccessResponse(info);
                 } catch (IOException e) {
@@ -278,41 +279,44 @@ public class OkHttpUtil {
         });
     }
 
-    private void onError(String message, ResultCallback callback) {
+    private void onNetError(String message, ResultCallback callback) {
         handler.post(() -> {
-            if(callback!=null){
-                callback.onError(message);
+            if (callback != null) {
+                callback.onNetError(message);
             }
         });
     }
 
-    private void onFailResponse(String info,ResultCallback callback) {
+    private void onFailCallback(String info, ResultCallback callback) {
         handler.post(() -> {
-            if(callback!=null){
+            if (callback != null) {
                 callback.onFailResponse(info);
             }
         });
     }
 
-    public void cancelTag(Object tag){
-        for (Call call : client.dispatcher().queuedCalls()){
-            if (tag.equals(call.request().tag())){
+    public void cancelTag(Object tag) {
+        for (Call call : client.dispatcher().queuedCalls()) {
+            if (tag.equals(call.request().tag())) {
                 call.cancel();
             }
         }
-        for (Call call : client.dispatcher().runningCalls()){
-            if (tag.equals(call.request().tag())){
+        for (Call call : client.dispatcher().runningCalls()) {
+            if (tag.equals(call.request().tag())) {
                 call.cancel();
             }
         }
     }
 
-    public void cancelAll(){
+    public void cancelAll() {
         client.dispatcher().cancelAll();
     }
-    public interface ResultCallback{
-        void onError(String message);
+
+    public interface ResultCallback {
+        void onNetError(String message);
+
         void onFailResponse(String info);
+
         void onSuccessResponse(String info) throws IOException;
     }
 }
--- app/src/main/java/com/example/palette/util/SPUtil.java
+++ app/src/main/java/com/example/palette/util/SPUtil.java
@@ -4,46 +4,93 @@ import android.content.Context;
 import android.content.SharedPreferences;
 
 public class SPUtil {
-    public static boolean put(Context context,String name,String key,Object value){
-        SharedPreferences preferences = context.getApplicationContext().getSharedPreferences(name,Context.MODE_PRIVATE);
+    private static Context mContext;
+    private static SPUtil mSpUtil;
+    private static String mName;
+
+    public static SPUtil init(Context context) {
+        mContext = context.getApplicationContext();
+        if (mSpUtil == null) {
+            synchronized (SPUtil.class) {
+                if (mSpUtil == null) {
+                    mSpUtil = new SPUtil();
+                }
+            }
+        }
+        return mSpUtil;
+    }
+
+    public static SPUtil setName(String name) {
+        if (mSpUtil == null) {
+            throw new RuntimeException("you must init before setName");
+        }
+        mName = name;
+        return mSpUtil;
+    }
+
+    private SPUtil() {
+
+    }
+
+    public static boolean put(String key, Object value) {
+        checkNameValid();
+        SharedPreferences preferences = mContext.getSharedPreferences(mName, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = preferences.edit();
-        if(value instanceof String){
+        if (value instanceof String) {
             editor.putString(key, (String) value);
-        }else if(value instanceof Integer){
+        } else if (value instanceof Integer) {
             editor.putInt(key, (Integer) value);
-        }else if(value instanceof Boolean){
+        } else if (value instanceof Boolean) {
             editor.putBoolean(key, (Boolean) value);
-        }else if(value instanceof Long){
+        } else if (value instanceof Long) {
             editor.putLong(key, (Long) value);
-        }else if(value instanceof Float){
+        } else if (value instanceof Float) {
             editor.putFloat(key, (Float) value);
-        }else {
-            editor.putString(key,value.toString());
+        } else {
+            editor.putString(key, value.toString());
         }
         return editor.commit();
     }
-    public static String getString(Context context,String name,String key,String defaultValue){
-        SharedPreferences preferences = context.getApplicationContext().getSharedPreferences(name,Context.MODE_PRIVATE);
-        return preferences.getString(key,defaultValue);
+
+    public static String getString(String key, String defaultValue) {
+        checkNameValid();
+        SharedPreferences preferences = mContext.getSharedPreferences(mName, Context.MODE_PRIVATE);
+        return preferences.getString(key, defaultValue);
     }
-    public static int getInt(Context context,String name,String key,int defaultValue){
-        SharedPreferences preferences = context.getApplicationContext().getSharedPreferences(name,Context.MODE_PRIVATE);
-        return preferences.getInt(key,defaultValue);
+
+    public static int getInt(String key, int defaultValue) {
+        checkNameValid();
+        SharedPreferences preferences = mContext.getSharedPreferences(mName, Context.MODE_PRIVATE);
+        return preferences.getInt(key, defaultValue);
     }
-    public static boolean getBoolean(Context context,String name,String key,boolean defaultValue){
-        SharedPreferences preferences = context.getApplicationContext().getSharedPreferences(name,Context.MODE_PRIVATE);
-        return preferences.getBoolean(key,defaultValue);
+
+    public static boolean getBoolean(String key, boolean defaultValue) {
+        checkNameValid();
+        SharedPreferences preferences = mContext.getSharedPreferences(mName, Context.MODE_PRIVATE);
+        return preferences.getBoolean(key, defaultValue);
     }
-    public static long getLong(Context context,String name,String key,long defaultValue){
-        SharedPreferences preferences = context.getApplicationContext().getSharedPreferences(name,Context.MODE_PRIVATE);
-        return preferences.getLong(key,defaultValue);
+
+    public static long getLong(String key, long defaultValue) {
+        checkNameValid();
+        SharedPreferences preferences = mContext.getSharedPreferences(mName, Context.MODE_PRIVATE);
+        return preferences.getLong(key, defaultValue);
     }
-    public static float getFloat(Context context,String name,String key,float defaultValue){
-        SharedPreferences preferences = context.getApplicationContext().getSharedPreferences(name,Context.MODE_PRIVATE);
-        return preferences.getFloat(key,defaultValue);
+
+    public static float getFloat(String key, float defaultValue) {
+        checkNameValid();
+        SharedPreferences preferences = mContext.getSharedPreferences(mName, Context.MODE_PRIVATE);
+        return preferences.getFloat(key, defaultValue);
     }
-    public static Object getObject(Context context,String name,String key,Object defaultValue){
-        SharedPreferences preferences = context.getApplicationContext().getSharedPreferences(name,Context.MODE_PRIVATE);
-        return preferences.getString(key,defaultValue.toString());
+
+    public static Object getObject(String key, Object defaultValue) {
+        checkNameValid();
+        SharedPreferences preferences = mContext.getSharedPreferences(mName, Context.MODE_PRIVATE);
+        return preferences.getString(key, defaultValue.toString());
+    }
+
+    private static void checkNameValid() {
+        if (mName == null) {
+            throw new RuntimeException("you must setName before put");
+        }
     }
 }
