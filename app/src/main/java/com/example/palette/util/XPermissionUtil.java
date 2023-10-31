67991a39c4a0cbb614fb01b05e3df7d0baf491a7
huachen <hc0507@126.com>
2023-10-31 13:21:23 +0800

    XXPermission工具回调优化

--- app/src/main/java/com/example/palette/util/XPermissionUtil.java
+++ app/src/main/java/com/example/palette/util/XPermissionUtil.java
@@ -2,7 +2,6 @@ package com.example.palette.util;
 
 import android.Manifest;
 import android.content.Context;
-import android.util.Log;
 
 import androidx.fragment.app.Fragment;
 
@@ -15,7 +14,9 @@ import java.util.List;
 import java.util.Map;
 
 public class XPermissionUtil {
+
     private static Map<String, String> map = new HashMap<>();
+
     static {
         map.put(Manifest.permission.CAMERA, "相机");
         map.put(Manifest.permission.WRITE_EXTERNAL_STORAGE, "写入外部存储");
@@ -28,59 +29,55 @@ public class XPermissionUtil {
         map.put(Manifest.permission.READ_SMS, "读取短信");
     }
 
-    public static void checkPermission(Context context, Fragment fragment,List<String> requestPermissions,XPermissionListener listener){
+    public static void checkPermission(Context context, Fragment fragment, List<String> requestPermissions, XPermissionListener listener) {
         if (context == null && fragment == null) {
             throw new NullPointerException("context and fragment must at least one can not be null");
         }
-        boolean granted = XXPermissions.isGranted(context==null?fragment.getActivity():context, requestPermissions);
+        boolean granted = XXPermissions.isGranted(context == null ? fragment.getActivity() : context, requestPermissions);
         List<String> requestNoGrantedList;
-        if(!granted){
-            requestNoGrantedList = XXPermissions.getDenied(context==null?fragment.getActivity():context, requestPermissions);
+        if (!granted) {
+            requestNoGrantedList = XXPermissions.getDenied(context == null ? fragment.getActivity() : context, requestPermissions);
             List<String> requestBeforeGrantedList = new ArrayList<>();
-            for(String request:requestPermissions){
-                if(!requestNoGrantedList.contains(request)){
+            for (String request : requestPermissions) {
+                if (!requestNoGrantedList.contains(request)) {
                     requestBeforeGrantedList.add(request);
                 }
             }
             XXPermissions xxPermissions;
-            if(context != null){
+            if (context != null) {
                 xxPermissions = XXPermissions.with(context);
-            }else {
+            } else {
                 xxPermissions = XXPermissions.with(fragment);
             }
             xxPermissions.permission(requestNoGrantedList).request(new OnPermissionCallback() {
                 @Override
                 public void onGranted(List<String> permissions, boolean allGranted) {
-                    if(allGranted && listener!=null){
-                        listener.permissionAllGranted(true);
+                    if (allGranted) {
+                        listener.permissionGranted(true, requestPermissions, permissionConvert(map, requestPermissions));
                     }
                 }
 
                 @Override
                 public void onDenied(List<String> permissions, boolean doNotAskAgain) {
-                    List<String> requestBeforeGrantedList = XXPermissions.getDenied(context == null ? fragment.getActivity() : context, requestPermissions);
-                    List<String> requestAfterGrantedList = new ArrayList<>();
-                    for(String request:requestPermissions){
-                        if(!requestBeforeGrantedList.contains(request)){
-                            requestAfterGrantedList.add(request);
+                    List<String> requestDeniedList = XXPermissions.getDenied(context == null ? fragment.getActivity() : context, requestPermissions);
+                    List<String> requestGrantedList = new ArrayList<>();
+                    for (String request : requestPermissions) {
+                        if (!requestDeniedList.contains(request)) {
+                            requestGrantedList.add(request);
                         }
                     }
-                    if(listener!=null){
-                        if(!requestAfterGrantedList.isEmpty()){
-                            listener.permissionGranted(requestAfterGrantedList,permissionConvert(map,requestAfterGrantedList));
-                        }
-                        if(doNotAskAgain){
-                            listener.permissionForeverDenied(requestBeforeGrantedList,permissionConvert(map,requestBeforeGrantedList));
-                        }else {
-                            listener.permissionDenied(requestBeforeGrantedList,permissionConvert(map,requestBeforeGrantedList));
-                        }
+                    if (!requestGrantedList.isEmpty()) {
+                        listener.permissionGranted(false, requestGrantedList, permissionConvert(map, requestGrantedList));
+                    }
+                    if (doNotAskAgain) {
+                        listener.permissionForeverDenied(requestDeniedList, permissionConvert(map, requestDeniedList));
+                    } else {
+                        listener.permissionDenied(requestDeniedList, permissionConvert(map, requestDeniedList));
                     }
                 }
             });
-        }else {
-            if(listener !=null){
-                listener.permissionAllGranted(false);
-            }
+        } else {
+            listener.permissionGranted(true, requestPermissions, permissionConvert(map, requestPermissions));
         }
     }
 
@@ -94,12 +91,10 @@ public class XPermissionUtil {
     }
 
     public interface XPermissionListener {
-        void permissionAllGranted(boolean doRequest);
+        void permissionGranted(boolean allGranted, List<String> grantedList, List<String> grantedChineseList);
 
-        void permissionGranted(List<String> grantedList,List<String> grantedChineseList);
+        void permissionDenied(List<String> deniedList, List<String> deniedChineseList);
 
-        void permissionDenied(List<String> deniedList,List<String> deniedChineseList);
-
-        void permissionForeverDenied(List<String> deniedList,List<String> deniedChineseList);
+        void permissionForeverDenied(List<String> deniedList, List<String> deniedChineseList);
     }
 }
